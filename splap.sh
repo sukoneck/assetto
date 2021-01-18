7c1c8e53b7e69043dcfb6885fe8ea9a43001f446
sukoneck <19413126+sukoneck@users.noreply.github.com>
2021-01-17 17:45:49 -0700

    Update splap.sh

--- splap.sh
+++ splap.sh
@@ -7,16 +7,21 @@ set -e
 # Set 'em up specific to ad hoc splapping. All other envs are already set by startup.sh
 set_variables() {
   echo ">>> Setting environment variables"
-  ASSETTO_PRESET=$1   # Which preset you finna pop? Should be a two digit number
-  KEEP_IT_CLEAN=$2    # 
-  PORT_HTTP=86${1}    # Assetto HTTP port. Must match your server_cfg.ini
-  PORT_TCPUDP=96${1}  # Assetto TCP/UPD port. Must match your server_cfg.ini
+  ASSETTO_PRESET=$1   # Two digit number that matches your (e.g.) ../Assetto/Server/Presets/SERVER_00 folder
+  KEEP_IT_CLEAN=$2    # Should existing containers get cleaned up?
+  PORT_HTTP=8081      # Assetto HTTP port. Must match your server_cfg.ini
+  PORT_TCPUDP=9600    # Assetto TCP/UPD port. Must match your server_cfg.ini
+  CONTAINER_IMAGE=    # Assetto server container image. You want this one "sukoneck/assetto:latest"
+  MOUNT_PATH_DEST=    # Path that will exist in the containers e.g. /mnt/assetto
+  MOUNT_PATH_SOURCE=  # Path that exists on the host e.g. /mnt/c/programfiles/Assetto/Server
+  STEAM_PASSWORD=     # Steam host password
+  STEAM_USERNAME=     # Steam host username
 }
 
 # Clean up containers if that's what you're into 
 clean_it_up() {
   if [ -n ${KEEP_IT_CLEAN} ]; then
-    echo ">>> Freshening up containers"
+    echo ">>> Freshening up all containers"
     docker stop $(sudo docker ps -aq)
     docker rm $(sudo docker ps -aq)
   else
@@ -27,7 +32,7 @@ clean_it_up() {
 # Pull the latest image from docker hub
 pull_container_image() {
   echo ">>> Getting the latest container image for ${CONTAINER_IMAGE}"
-  docker pull ${CONTAINER_IMAGE}
+  docker pull ${CONTAINER_IMAGE} -q
 }
 
 # Go on, get
@@ -35,9 +40,9 @@ run_container() {
   echo ">>> Starting Assetto server container"
   docker run -d \
     -v ${MOUNT_PATH_SOURCE}/content:${MOUNT_PATH_DEST}/content \
-    -v ${MOUNT_PATH_SOURCE}/cfg:${MOUNT_PATH_DEST}/cfg \
+    -v ${MOUNT_PATH_SOURCE}/Presets/SERVER_${ASSETTO_PRESET}:${MOUNT_PATH_DEST}/cfg \
     -e ASSETTO_CFG=${ASSETTO_PRESET} \
-    -e MOUNT_PATH_DEST=${MOUNT_PATH_DEST}
+    -e MOUNT_PATH_DEST=${MOUNT_PATH_DEST} \
     -e STEAM_USERNAME=${STEAM_USERNAME} \
     -e STEAM_PASSWORD=${STEAM_PASSWORD} \
     -p ${PORT_TCPUDP}:${PORT_TCPUDP}/tcp \
