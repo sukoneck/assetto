feb8a9c9d1419b4072753768dd2be2ba9e3dc8d9
sukoneck <19413126+sukoneck@users.noreply.github.com>
2021-01-17 18:14:16 -0700

    the abandoning of the armada retires assetto_cfg

--- entry.sh
+++ entry.sh
@@ -32,7 +32,7 @@ install_assetto() {
 
 config_assetto() {
   echo ">>> Configuring assetto using cfg files"
-  cp ${MOUNT_PATH}/cfg/SERVER_${ASSETTO_CFG}/* ${STEAM_PATH}/assetto/cfg/
+  cp ${MOUNT_PATH}/cfg/* ${STEAM_PATH}/assetto/cfg/
   rm -rf ${STEAM_PATH}/assetto/content
   ln -s ${MOUNT_PATH}/content ${STEAM_PATH}/assetto/content
   chmod -R 755 ${STEAM_PATH}/
